be159027bb9b2f656d854d3bb843d02a49aafc15
Raj Shah <raj@wohlig.com>
2019-11-23 21:14:22 +0530

    image issue

--- src/filters/filter.js
+++ src/filters/filter.js
@@ -1,25 +1,26 @@
-import Vue from "vue";
+import Vue from 'vue';
+import globalJs from '@/service/global.js';
 
-Vue.filter("serverimage", (value, width, height, style) => {
-  let tmp = function(input, width, height, style) {
+Vue.filter('serverimage', (value, width, height, style) => {
+  const tmp = function (input, width, height, style) {
     if (input) {
-      if (input.substr(0, 4) == "http") {
+      if (input.substr(0, 4) == 'http') {
         return input;
       }
-      var image = "http://wohlig.io:1330/api/Upload/readfile" + "?file=" + input;
+      let image = globalJs.readFileUrl + input;
+      // var image = "http://wohlig.io:1330/api/Upload/readfile" + "?file=" + input;
       if (width) {
-        image += "&width=" + width;
+        image += '&width=' + width;
       }
       if (height) {
-        image += "&height=" + height;
+        image += '&height=' + height;
       }
       if (style) {
-        image += "&style=" + style;
+        image += '&style=' + style;
       }
       return image;
-    } else {
-      return "img/mtc-logo.png";
     }
+    return 'img/mtc-logo.png';
   };
   return tmp(value, width, height, style);
 });
